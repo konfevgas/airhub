c143656fd1c5726c3f30e2f3d02a49ba943d84a1
Konstantinos Fevgas <kon.fevgas@gmail.com>
2025-11-17 18:28:08 +0200

    Add description

--- backend/tests/test_unit/test_utils/test_sql_utils/test_check_user_exists.py
+++ backend/tests/test_unit/test_utils/test_sql_utils/test_check_user_exists.py
@@ -5,5 +5,8 @@ session = next(get_db())
 
 
 def test_check_user_exists() -> None:
-    user_exists = check_user_exists(user_email="fesas1988@gmail.com", session=session)
+    """
+    Add description here.
+    """
+    user_exists = check_user_exists(user_email="fesas198@gmail.com", session=session)
     assert user_exists is True
