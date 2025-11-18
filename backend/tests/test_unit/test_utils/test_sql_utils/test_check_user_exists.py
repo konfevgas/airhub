1d93d8686a8960193ace444fdf57af5f8798ee1e
Konstantinos Fevgas <kon.fevgas@gmail.com>
2025-11-18 07:10:22 +0200

    MInor fix

--- backend/tests/test_unit/test_utils/test_sql_utils/test_check_user_exists.py
+++ backend/tests/test_unit/test_utils/test_sql_utils/test_check_user_exists.py
@@ -6,7 +6,7 @@ session = next(get_db())
 
 def test_check_user_exists() -> None:
     """
-    Add description here.
+    Test the check_user_exists function to verify it correctly identifies existing users.
     """
     user_exists = check_user_exists(user_email="fesas198@gmail.com", session=session)
     assert user_exists is True
